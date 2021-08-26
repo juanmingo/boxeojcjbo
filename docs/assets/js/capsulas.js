ed87f7176dd3eb6cb6652efd7ceb215077053006
Juan Delgado <juan.delgador@gmail.com>
2021-08-25 23:38:45 -0400

    Capsulas SO I

---
+++ docs/assets/js/capsulas.js
@@ -0,0 +1,51 @@
+$('#capsula_i').on('hidden.bs.modal', function(e) {
+    $('#capsula_i iframe').attr("src", $("#capsula_i iframe").attr("src"));
+});
+
+$('#capsula_ii').on('hidden.bs.modal', function(e) {
+    $('#capsula_ii iframe').attr("src", $("#capsula_ii iframe").attr("src"));
+});
+
+$('#capsula_iii').on('hidden.bs.modal', function(e) {
+    $('#capsula_iii iframe').attr("src", $("#capsula_iii iframe").attr("src"));
+});
+
+$('#capsula_iv').on('hidden.bs.modal', function(e) {
+    $('#capsula_iv iframe').attr("src", $("#capsula_iv iframe").attr("src"));
+});
+
+$('#capsula_v').on('hidden.bs.modal', function(e) {
+    $('#capsula_v iframe').attr("src", $("#capsula_v iframe").attr("src"));
+});
+
+$('#capsula_vi').on('hidden.bs.modal', function(e) {
+    $('#capsula_vi iframe').attr("src", $("#capsula_vi iframe").attr("src"));
+});
+
+$('#capsula_vii').on('hidden.bs.modal', function(e) {
+    $('#capsula_vii iframe').attr("src", $("#capsula_vii iframe").attr("src"));
+});
+
+$('#capsula_viii').on('hidden.bs.modal', function(e) {
+    $('#capsula_viii iframe').attr("src", $("#capsula_viii iframe").attr("src"));
+});
+
+$('#capsula_ix').on('hidden.bs.modal', function(e) {
+    $('#capsula_ix iframe').attr("src", $("#capsula_ix iframe").attr("src"));
+});
+
+$('#capsula_x').on('hidden.bs.modal', function(e) {
+    $('#capsula_x iframe').attr("src", $("#capsula_x iframe").attr("src"));
+});
+
+$('#capsula_xi').on('hidden.bs.modal', function(e) {
+    $('#capsula_xi iframe').attr("src", $("#capsula_xi iframe").attr("src"));
+});
+
+$('#capsula_xii').on('hidden.bs.modal', function(e) {
+    $('#capsula_xii iframe').attr("src", $("#capsula_xii iframe").attr("src"));
+});
+
+$('#capsula_xiii').on('hidden.bs.modal', function(e) {
+    $('#capsula_xiii iframe').attr("src", $("#capsula_xiii iframe").attr("src"));
+});
